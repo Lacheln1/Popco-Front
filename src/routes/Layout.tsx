c34d7b1833327879e872916dd85bfe55652b74b0
Lacheln <psionict@naver.com>
2025-07-17 01:00:58 +0900

    chore: footer 소문자>대문자 변경

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/common/footer";
+import Footer from "../components/common/Footer";
 import Header from "@/components/common/Header";
 const Layout = () => {
   return (
