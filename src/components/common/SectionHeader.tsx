efa1d585a692509a8dbded5b86bed03455e3bbfc
yeeun426 <thsudkcla7@naver.com>
2025-07-16 11:31:27 +0900

    [POP-120] Feat: tailwind prettier 적용 & Section Header 반응형

--- src/components/common/SectionHeader.tsx
+++ src/components/common/SectionHeader.tsx
@@ -2,20 +2,10 @@ interface SectionHeaderProps {
   title: string;
   description: string;
 }
-
 const SectionHeader = ({ title, description }: SectionHeaderProps) => {
   return (
-    <header className="h-[450px] bg-[#172039] text-white relative">
-      <section
-        className="
-      w-full px-4
-      sm:w-[640px]
-      md:w-[768px]
-      lg:w-[1024px]
-      xl:w-[1200px]
-      m-auto pt-[120px] sm:pt-[150px] md:pt-[180px] lg:pt-[200px]
-    "
-      >
+    <header className="relative h-[250px] bg-[#172039] text-white sm:h-[350px] md:h-[400px] lg:h-[450px]">
+      <section className="m-auto w-full px-4 pt-[100px] sm:w-[640px] sm:pt-[120px] md:w-[768px] md:pt-[160px] lg:w-[1024px] lg:pt-[200px] xl:w-[1200px]">
         <h2 className="gmarket-medium text-xl sm:text-2xl md:text-3xl lg:text-4xl">
           {title}
         </h2>
@@ -23,49 +13,27 @@ const SectionHeader = ({ title, description }: SectionHeaderProps) => {
           {description}
         </h5>
       </section>
-
       <img
-        className="
-      mix-blend-screen absolute
-      top-[6rem] left-[2rem]
-      md:top-[10rem] md:left-[5rem]
-      lg:top-[12rem] lg:left-[7rem]
-    "
+        className="absolute left-[2rem] top-[1rem] w-[80px] mix-blend-screen sm:left-[5rem] sm:top-[5rem] sm:w-[100px] md:left-[7rem] md:top-[10rem] md:w-[120px] lg:left-[7rem] lg:top-[12rem] lg:w-[150px]"
         src="/images/components/glossy_popcorn.png"
-        alt="glass"
+        alt="popcorn"
       />
       <img
-        className="
-      mix-blend-screen absolute
-      top-[1rem] left-[20rem]
-      md:top-[2rem] md:left-[40rem]
-      lg:top-[2rem] lg:left-[60rem]
-    "
+        className="absolute left-[12rem] top-[1rem] w-[100px] mix-blend-screen sm:left-[25rem] sm:top-[2rem] sm:w-[100px] md:w-[130px] lg:left-[57%] lg:top-[2rem] lg:w-[160px]"
         src="/images/components/glossy_glass.png"
         alt="glass"
       />
       <img
-        className="
-      mix-blend-screen absolute
-      top-[12rem] right-[10rem]
-      md:top-[15rem] md:right-[20rem]
-      lg:top-[15rem] lg:right-[30rem]
-    "
+        className="absolute right-[5rem] top-[10rem] w-[70px] mix-blend-screen sm:right-[10rem] sm:top-[12rem] sm:w-[70px] md:right-[20rem] md:top-[15rem] md:w-[100px] lg:right-[30rem] lg:top-[15rem] lg:w-[120px]"
         src="/images/components/glossy_slate.png"
-        alt="glass"
+        alt="slate"
       />
       <img
-        className="
-      mix-blend-screen absolute
-      top-[4rem] right-[2rem]
-      md:top-[5rem] md:right-[3rem]
-      lg:top-[6rem] lg:right-[5rem]
-    "
+        className="absolute right-[1rem] top-[4rem] w-[80px] mix-blend-screen sm:right-[2rem] sm:top-[5rem] sm:w-[100px] md:right-[3rem] md:top-[6rem] md:w-[130px] lg:right-[5rem] lg:top-[6rem] lg:w-[160px]"
         src="/images/components/glossy_tv.png"
-        alt="glass"
+        alt="tv"
       />
     </header>
   );
 };
-
 export default SectionHeader;
--- src/pages/TestMainPage.tsx
+++ src/pages/TestMainPage.tsx
@@ -1,14 +1,5 @@
-import SectionHeader from "@/components/common/SectionHeader";
-
 const TestMainPage = () => {
-  return (
-    <div>
-      <SectionHeader
-        title="컬렉션"
-        description="보고 싶은 콘텐츠, 추천하고 싶은 시리즈, 그리고 나만의 테마까지 OTT 컬렉션을 만들어 공유해보세요."
-      />
-    </div>
-  );
+  return <div>테스트</div>;
 };
 
 export default TestMainPage;
