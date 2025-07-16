82ed0a5667f174e622aaf5702986d5d9e5e4cb7e
Lacheln <psionict@naver.com>
2025-07-16 23:03:35 +0900

    test: 헤더 탭 이동 확인을 위한 테스트 페이지 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import TestMainPage from "@/pages/TestMainPage";
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [{ index: true, element: <TestMainPage /> }],
-    },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <TestMainPage /> },
+      { path: "/list", element: <TestMainPage /> },
+      { path: "/collections", element: <TestMainPage /> },
+      { path: "/analysis", element: <TestMainPage /> },
+      { path: "/login", element: <TestMainPage /> },
+    ],
+  },
 ]);
 
 export default router;
