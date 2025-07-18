76e81a16a64122872589af344626828e9ba8867b
lacheln <psionict@naver.com>
2025-07-18 15:05:41 +0900

    feat: 로그인 페이지 구조 제작중

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import spotlightImg from "@/assets/spotlight.svg";
+import spotlightWithLogoImg from "@/assets/spotlight-with-logo.svg";
+import loginPopcoImg from "@/assets/login-popco.svg";
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <div className="flex">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
