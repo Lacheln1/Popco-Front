bb9faea5eca9320b71407d66337e2475e4212ded
lacheln <psionict@naver.com>
2025-07-18 15:06:08 +0900

    feat: 헤더&푸터 없는 페이지를 위한 레이아웃 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import TestMainPage from "@/pages/TestMainPage";
+import LoginPage from "@/pages/LoginPage";
+import TestLayout from "./TestLayout";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <TestMainPage /> },
+      { index: true, element: <LoginPage /> },
       { path: "/list", element: <TestMainPage /> },
       { path: "/collections", element: <TestMainPage /> },
       { path: "/analysis", element: <TestMainPage /> },
       { path: "/login", element: <TestMainPage /> },
     ],
+  },
+  {
+    //Header/Footer가 없는 테스트 페이지
+    element: <TestLayout />,
+    children: [{ path: "/preference-test", element: <TestPage /> }],
   },
 ]);
 
