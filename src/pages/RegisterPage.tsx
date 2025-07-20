0759098d27eb848740d5a2fe75c53734d8c3bd58
Lacheln <psionict@naver.com>
2025-07-20 23:32:41 +0900

    design: 태블릿, 모바일 레이아웃 제작 및 완성

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -113,6 +113,100 @@ const RegisterPage: React.FC = () => {
           </div>
         </div>
       </div>
+
+      {/* 모바일 */}
+      <div className="flex flex-col lg:hidden">
+        {/* 로고 이미지 */}
+        <div className="relative flex flex-1 flex-col items-center justify-center">
+          <a href="/">
+            <img
+              src={spotlightWithLogoImg}
+              alt="스포트라이트로고"
+              className="w-64 object-cover"
+            />
+          </a>
+        </div>
+
+        {/* 회원가입 섹션 */}
+        <div className="flex justify-center px-4 pb-4 md:mx-10">
+          <div className="b-gray-100 w-[600px] rounded-3xl bg-gray-100 p-6 opacity-95 shadow-2xl md:mt-14">
+            <div className="mb-6 text-center">
+              <h1 className="text-xl font-bold text-gray-800">
+                POPCO 멤버가 되어
+              </h1>
+              <h1 className="mb-2 text-xl font-bold text-gray-800">
+                특별한 경험을 만나보세요.
+              </h1>
+              <p className="text-sm text-gray-600">회원가입 하시겠어요?</p>
+            </div>
+
+            <form className="flex flex-col items-center space-y-3">
+              <div className="w-full max-w-[440px]">
+                <label className="mb-2 ml-4 block text-sm text-black">
+                  이메일
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email Address"
+                  className="w-full rounded-[40px] border-0 bg-white px-3 py-3 text-sm text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#ffd751]"
+                />
+              </div>
+
+              <div className="w-full max-w-[440px]">
+                <label className="mb-2 ml-4 block text-sm text-black">
+                  비밀번호
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full rounded-[40px] border-0 bg-white px-3 py-3 text-sm text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#ffd751]"
+                />
+                <p className="text-popcorn-box ml-4 mt-2 text-xs">
+                  올바른 형식이 아닙니다
+                </p>
+              </div>
+
+              <div className="w-full max-w-[440px]">
+                <label className="mb-2 ml-4 block text-sm text-black">
+                  비밀번호 확인
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full rounded-[40px] border-0 bg-white px-3 py-3 text-sm text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#ffd751]"
+                />
+                <p className="text-popcorn-box ml-4 mt-2 text-xs">
+                  일치하지 않습니다
+                </p>
+              </div>
+
+              <div className="w-full max-w-[500px] pt-3 md:pt-4">
+                <button
+                  type="submit"
+                  className="bg-popco-main w-full rounded-[400px] px-4 py-3 text-xl font-medium text-black transition-colors hover:bg-yellow-400 md:py-4"
+                >
+                  회원가입
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 flex justify-center lg:mt-6">
+              <p className="text-xs text-gray-600 lg:text-base">
+                이미 회원이신가요?{" "}
+                <a
+                  href="/login"
+                  className="text-sunglasses-deepBlue ml-1 hover:text-[#142c44]"
+                >
+                  로그인 하기
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* 하단 - 캐릭터들 */}
+        <div className="flex justify-center pb-8">
+          <img src={loginPopcoImg} alt="" className="w-[600px]" />
+        </div>
+      </div>
     </main>
   );
 };
