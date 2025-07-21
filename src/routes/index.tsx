668fcbff60aba2106af2880245fb30d4bf84f5a1
Lacheln1 <59949555+Lacheln1@users.noreply.github.com>
2025-07-21 10:02:24 +0900

    chore: route index conflict로 인한 경로 수정2
    기존에 있던 테스트 레이아웃 및 관련 파일 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
-import TestLayout from "@/layout/TestLayout";
-import TestMainPage from "@/pages/TestMainPage";
-import TestPage from "@/pages/TestPage"; // 선호도 진단 페이지
-import MainPage from "@/pages/MainPage";
-
+import LoginPage from "@/pages/LoginPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,11 +12,6 @@ const router = createBrowserRouter([
       { path: "/analysis", element: <MainPage /> },
       { path: "/login", element: <MainPage /> },
     ],
-  },
-  {
-    //Header/Footer가 없는 테스트 페이지
-    element: <TestLayout />,
-    children: [{ path: "/preference-test", element: <TestPage /> }],
   },
 ]);
 
