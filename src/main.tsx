274c15aff3f360031c36fb09882a33a8758fb9fd
zeromin41 <young@YoungminGuri>
2025-07-22 15:46:48 +0900

    fix: 디자인 및 svg 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index";
-
-import { App as AntApp, ConfigProvider } from 'antd'; 
-import koKR from 'antd/locale/ko_KR'; //antd 한국어팩 추가
-import 'dayjs/locale/ko'; 
-import dayjs from 'dayjs'; 
-
+import { App as AntApp } from 'antd';
 import 'antd/dist/reset.css';
 import "./index.css";
 
-dayjs.locale('ko');
-
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ConfigProvider locale={koKR}>
-            <AntApp>
-                <RouterProvider router={router} />
-            </AntApp>
-        </ConfigProvider>
+        <AntApp>
+            <RouterProvider router={router} />
+        </AntApp>
     </StrictMode>
 );
