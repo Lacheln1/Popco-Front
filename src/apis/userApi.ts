20d843818d0ba1269a4b17df48d0eab68ca3aaa9
Lacheln <psionict@naver.com>
2025-07-22 22:43:14 +0900

    chore: 로그인url 수정

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { P } from "node_modules/framer-motion/dist/types.d-Bq-Qm38R";
 
 //백엔드 url 넣어야함
 const API_URL = `${import.meta.env.VITE_BACK_URL}`;
@@ -10,15 +9,14 @@ interface LoginParams {
 }
 
 export const getUserInfo = async () => {
-  //백엔드 getUser url 넣어야함
-  const response = await axios.get(`${API_URL}`);
+  const response = await axios.get(`${API_URL}/users`);
   return response.data;
 };
 
 export const loginUser = async ({ email, password }: LoginParams) => {
   //백엔드 로그인 url 넣어야함
   const response = await axios.post(
-    `${API_URL}`,
+    `${API_URL}/api/client/auth/login`,
     {
       email,
       password,
