9a4f381ee9be529d825351836860d63a8106829b
Lacheln <psionict@naver.com>
2025-07-23 00:44:52 +0900

    feat: 페르소나 페이지 구조 작성 시작

---
+++ src/pages/AnalysisPage.tsx
@@ -0,0 +1,11 @@
+import AnalysisHeroSection from "@/components/Analysis/AnalysisHeroSection";
+
+const AnalysisPage = () => {
+  return (
+    <main className="pretendard">
+      <AnalysisHeroSection />
+    </main>
+  );
+};
+
+export default AnalysisPage;
