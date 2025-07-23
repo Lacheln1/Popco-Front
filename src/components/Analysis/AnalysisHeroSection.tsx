8a0269a3ad5283f86db7f6b09cce42d84686759d
Lacheln <psionict@naver.com>
2025-07-24 00:43:03 +0900

    design: title 및 캐릭터 반응형 구현중

--- src/components/Analysis/AnalysisHeroSection.tsx
+++ src/components/Analysis/AnalysisHeroSection.tsx
@@ -1,41 +1,44 @@
 const AnalysisHeroSection = () => {
   return (
     <div>
-      <div className="relative h-[300px] overflow-hidden xl:h-[650px]">
+      <div className="relative h-[380px] overflow-hidden">
         <div
           className="bg-footerBlue absolute inset-0"
-          style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 85%)" }} //왼쪽 상단을 기준으로(x,y), 오른쪽 상단(x,y), 오른쪽 하단, 왼쪽 하단
+          style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 80%)" }} //왼쪽 상단을 기준으로(x,y), 오른쪽 상단(x,y), 오른쪽 하단, 왼쪽 하단
         ></div>
 
         {/* 컨텐츠 */}
         <div className="relative mx-auto h-full w-full">
           {/* 메인 텍스트 */}
-          <div className="absolute left-1/2 top-32 -translate-x-1/2 transform">
-            <h1 className="gmarket-medium text-4xl text-white">
+          <div className="absolute left-1/2 top-32 w-60 -translate-x-1/2 transform">
+            <h1 className="gmarket-medium text-center text-2xl text-white">
               POPCO가 알려주는 나의 OTT 취향은?
             </h1>
           </div>
 
           {/* 캐릭터 이미지 및 텍스트 */}
-          <div className="flex justify-center pt-56">
-            <div className="flex flex-col pt-4">
-              <span className="bg-popco-main gmarket-medium mb-2 px-2">
-                심장 쫄깃한 전개와 스케일 없는 세계관 없이는 못참아!
-              </span>
-              <img
-                src="/images/persona/Persona-text-line.svg"
-                alt="텍스트 라인"
-              />
-            </div>
-            <div className="flex justify-center">
+          <div className="flex pt-56">
+            <div className="mx-auto flex">
+              <div className="absolute flex -translate-x-80 flex-col pt-4">
+                <span className="bg-popco-main gmarket-medium mb-2 px-2 py-1">
+                  심장 쫄깃한 전개와 스케일 없는 세계관 없이는 못참아!
+                </span>
+                <img
+                  src="/images/persona/Persona-text-line.svg"
+                  alt="텍스트 라인"
+                />
+              </div>
               <img
                 src="/images/persona/애기액션헌터.png"
                 alt="캐릭터이미지"
-                className="h-[350px] w-[350px] object-cover"
+                className="h-[150px] w-[150px] object-cover"
               />
             </div>
           </div>
         </div>
+      </div>
+      <div className="gmarket-medium flex justify-center text-4xl">
+        애기 액션 헌터
       </div>
     </div>
   );
