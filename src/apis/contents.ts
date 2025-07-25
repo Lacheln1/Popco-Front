ebafed4fe927b55ef6eb4a5909e7fc88d408277b
yeeun426 <thsudkcla7@naver.com>
2025-07-25 13:12:10 +0900

    chore : import 수정

--- src/apis/contents.ts
+++ src/apis/contents.ts
@@ -1,10 +1,10 @@
 import { ContentCategory, ContentItem } from "@/types/Contents.types";
-import axios from "./axiosInstance";
+import axiosInstance from "./axiosInstance";
 
 // 주간 랭킹
 export const fetchContentsRanking = async (
   type: ContentCategory,
 ): Promise<ContentItem[]> => {
-  const { data } = await axios.get(`/contents/popular/type/${type}`);
+  const { data } = await axiosInstance.get(`/contents/popular/type/${type}`);
   return data;
 };
