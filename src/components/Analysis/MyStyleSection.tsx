4589896afbe76b41758e5b204bebd239b75b347f
Lacheln <psionict@naver.com>
2025-07-27 04:42:30 +0900

    design: PC환경 반응형 수정
    max-width 변경 및 이에 따른 폰트 크기 수정

--- src/components/Analysis/MyStyleSection.tsx
+++ src/components/Analysis/MyStyleSection.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const MyStyleSection: React.FC = () => {
   return (
-    <section className="pretendard flex">
+    <section className="pretendard flex justify-center">
       <div
-        className="mx-1 flex w-full flex-col bg-slate-50 py-5"
+        className="mx-1 flex w-full max-w-[1200px] flex-col bg-slate-50 py-5"
         style={{ boxShadow: "0 0px 10px rgba(0, 0, 0, 0.1)" }}
       >
-        <div className="flex flex-col text-center text-base md:text-2xl lg:text-3xl">
+        <div className="flex flex-col text-center text-base md:text-2xl lg:text-2xl">
           <span className="gmarket-medium">내 OTT 시청 스타일은</span>
           <span className="">상위 8.2% 의 #스릴러, 상위 8.3% 의 액션러!</span>
         </div>
-        <div className="flex w-full justify-center gap-6 pt-4 md:text-xl lg:text-2xl">
+        <div className="flex w-full justify-center gap-6 pt-4 md:text-xl">
           <div className="bg-popco-main flex h-8 w-16 items-center justify-center rounded-lg md:w-24 lg:h-10 lg:w-28">
             #액션
           </div>
@@ -23,7 +23,7 @@ const MyStyleSection: React.FC = () => {
           </div>
         </div>
         <div className="mt-4 flex justify-center break-keep px-1 text-center">
-          <span className="break-keep md:text-xl xl:text-2xl">
+          <span className="break-keep md:text-xl">
             당신은 숨쉴 틈 없는 전개와 화려한 액션을 즐기며,손에 땀을 쥐는
             긴장감과 거대한 스케일 속에 완전히 빠져드는 스타일이네요!
           </span>
