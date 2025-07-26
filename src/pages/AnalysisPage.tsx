7818f3bef1712e9e24bfdacb6e7f32b187df752a
Lacheln <psionict@naver.com>
2025-07-27 03:28:22 +0900

    feat: 시청 스타일 section 제작

--- src/pages/AnalysisPage.tsx
+++ src/pages/AnalysisPage.tsx
@@ -1,9 +1,11 @@
 import AnalysisHeroSection from "@/components/Analysis/AnalysisHeroSection";
+import MyStyleSection from "@/components/Analysis/MyStyleSection";
 
 const AnalysisPage = () => {
   return (
     <main className="pretendard">
       <AnalysisHeroSection />
+      <MyStyleSection />
     </main>
   );
 };
