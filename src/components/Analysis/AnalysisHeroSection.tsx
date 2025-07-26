29eace0185e5ce271c21f292f5a85ad09871d420
Lacheln <psionict@naver.com>
2025-07-27 01:48:04 +0900

    design: xl사이즈 반응형 구현

--- src/components/Analysis/AnalysisHeroSection.tsx
+++ src/components/Analysis/AnalysisHeroSection.tsx
@@ -1,7 +1,7 @@
 const AnalysisHeroSection = () => {
   return (
-    <div>
-      <div className="relative h-[380px] overflow-hidden md:h-[420px] lg:h-[480px]">
+    <div className="pretendard">
+      <div className="relative h-[380px] overflow-hidden md:h-[425px] lg:h-[480px]">
         <div
           className="bg-footerBlue absolute inset-0"
           style={{ clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 80%)" }} //왼쪽 상단을 기준으로(x,y), 오른쪽 상단(x,y), 오른쪽 하단, 왼쪽 하단
@@ -19,14 +19,14 @@ const AnalysisHeroSection = () => {
           {/* 캐릭터 이미지 및 텍스트 */}
           <div className="flex pt-56">
             <div className="mx-auto flex">
-              <div className="absolute flex -translate-x-[97px] -translate-y-10 flex-col pt-4 md:-translate-x-60 lg:-translate-x-80">
+              <div className="absolute flex -translate-x-[97px] -translate-y-10 flex-col pt-4 md:-translate-x-60 lg:-translate-x-80 xl:-translate-x-[450px] xl:pt-12">
                 <span className="gmarket-medium md:bg-popco-main mb-2 whitespace-nowrap px-1 py-1 text-white md:text-xl md:text-black">
                   심장 쫄깃한 전개와 스케일 없는 세계관 없이는 못참아!
                 </span>
                 <img
                   src="/images/persona/Persona-text-line.svg"
                   alt="텍스트 라인"
-                  className="hidden object-cover md:inline md:w-60 lg:ml-24"
+                  className="hidden object-cover md:inline md:w-60 lg:ml-24 xl:ml-56"
                 />
               </div>
               <img
@@ -40,6 +40,45 @@ const AnalysisHeroSection = () => {
       </div>
       <div className="gmarket-medium flex justify-center text-3xl md:mt-8 md:text-4xl lg:mt-0">
         애기 액션 헌터
+      </div>
+
+      <div className="flex justify-around">
+        <div className="mt-3 flex w-full items-center px-1 md:max-w-[700px] lg:max-w-[1200px]">
+          <div>
+            <div className="flex justify-center">
+              <img
+                src="/public/images/persona/애기액션헌터.png"
+                alt="액션 헌터"
+                className="h-16 w-16"
+              />
+            </div>
+            <span className="text-sm md:text-lg lg:text-xl xl:text-2xl">
+              액션 헌터
+            </span>
+          </div>
+          <div className="mx-2 flex h-4 flex-1 overflow-hidden rounded-full bg-gray-200">
+            <div className="h-full bg-red-400" style={{ width: "52%" }}></div>
+            <div className="h-full bg-blue-400" style={{ width: "48%" }}></div>
+          </div>
+          <div className="">
+            <div className="flex justify-center">
+              <img
+                src="/public/images/persona/무비셜록-아기.svg"
+                alt="온기 수집가"
+                className="h-16 w-16"
+              />
+            </div>
+            <span className="text-sm md:text-lg lg:text-xl xl:text-2xl">
+              온기 수집가
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="relative -top-5 flex w-full justify-around px-1 md:max-w-[700px] lg:max-w-[1200px]">
+          <div className="md:text-lg lg:text-xl xl:text-2xl">52%</div>
+          <div className="md:text-lg lg:text-xl xl:text-2xl">48%</div>
+        </div>
       </div>
     </div>
   );
