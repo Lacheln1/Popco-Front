49c897116badd4675146951d6f758272fb429385
Lacheln <psionict@naver.com>
2025-07-27 20:01:56 +0900

    feat: 나와 같은 ' ' 들은? section 추가
    반응형 다 구현 완료

--- src/pages/AnalysisPage.tsx
+++ src/pages/AnalysisPage.tsx
@@ -1,11 +1,13 @@
 import AnalysisHeroSection from "@/components/Analysis/AnalysisHeroSection";
 import MyStyleSection from "@/components/Analysis/MyStyleSection";
+import RoleDashBoard from "@/components/Analysis/RoleDashBoard";
 
 const AnalysisPage = () => {
   return (
     <main className="pretendard">
       <AnalysisHeroSection />
       <MyStyleSection />
+      <RoleDashBoard />
     </main>
   );
 };
