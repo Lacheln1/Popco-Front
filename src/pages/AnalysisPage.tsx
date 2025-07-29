63d17fabd6df4ac673d3800cee70868e862be210
Lacheln <psionict@naver.com>
2025-07-29 21:36:58 +0900

    feat: 유저 페르소나 로직 작성중

--- src/pages/AnalysisPage.tsx
+++ src/pages/AnalysisPage.tsx
@@ -1,10 +1,28 @@
+import { getUserPersonas } from "@/apis/userApi";
 import AnalysisHeroSection from "@/components/Analysis/AnalysisHeroSection";
 import LikeContentSection from "@/components/Analysis/LikeContentsSection";
 import MyStyleSection from "@/components/Analysis/MyStyleSection";
 import MyWatchingStyleBoard from "@/components/Analysis/MyWatchingStyleBoard";
 import RoleDashBoard from "@/components/Analysis/RoleDashBoard";
+import useAuthCheck from "@/hooks/useAuthCheck";
+import { useEffect, useState } from "react";
 
 const AnalysisPage = () => {
+  const [userData, setUserData] = useState("");
+  const { accessToken } = useAuthCheck();
+  useEffect(() => {
+    const getUserPersonasData = async () => {
+      if (!accessToken) return;
+      try {
+        const response = await getUserPersonas(accessToken);
+        setUserData(response);
+      } catch (error) {
+        console.error("페르소나 데이터 가져오기 실패:", error);
+      }
+    };
+    getUserPersonasData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <main className="pretendard">
       <AnalysisHeroSection />
