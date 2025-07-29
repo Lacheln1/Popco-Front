6e8d7b415fd89b785d088207cae297ea6f3d5021
Lacheln <psionict@naver.com>
2025-07-29 21:36:42 +0900

    feat: 유저 페르소나 api 함수 작성

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -75,3 +75,20 @@ export const getUserDetail = async (accessToken: string) => {
     throw error;
   }
 };
+
+export const getUserPersonas = async (accessToken: string) => {
+  try {
+    const response = await axios.get(`${API_URL}/personas`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      withCredentials: true,
+    });
+
+    console.log("유저 페르소나 get", response.data);
+
+    return response.data;
+  } catch (error) {
+    console.log("getPersonas실패", error);
+  }
+};
