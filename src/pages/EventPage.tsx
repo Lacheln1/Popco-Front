9a12b0b6f63be8ca358dec7632b12767a9a33ea6
yeeun426 <thsudkcla7@naver.com>
2025-07-31 16:24:52 +0900

    style : 반응형

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -89,8 +89,8 @@ const EventPage = () => {
         }}
       >
         {/* 카드 */}
-        <div className="relative h-screen pt-20">
-          <aside className="absolute left-1/2 top-1/2 z-10 flex w-[85%] -translate-x-1/2 -translate-y-1/3 flex-col items-center justify-center break-keep rounded-xl bg-white/80 px-4 py-8 shadow-xl backdrop-blur-md md:top-1/3 md:h-[450px] md:w-[800px] md:px-8">
+        <div className="relative mt-10 h-screen pt-20 md:mt-0">
+          <aside className="absolute left-1/2 top-1/3 z-10 flex w-[85%] -translate-x-1/2 -translate-y-1/3 flex-col items-center justify-center break-keep rounded-xl bg-white/80 px-4 py-8 shadow-xl backdrop-blur-md md:h-[450px] md:w-[800px] md:px-8">
             <h3 className="gmarket md: mb-1 text-center text-xl font-medium tracking-tight text-gray-900 md:text-[1.75rem]">
               퀴즈 이벤트가 곧 시작됩니다!
             </h3>
@@ -99,7 +99,7 @@ const EventPage = () => {
               renderer={renderCount}
             />
             <div className="flex flex-col gap-2">
-              <div className="pretendard flex items-center gap-3">
+              <div className="pretendard flex flex-row items-center gap-3">
                 <span className="bg-footerBlue w-[90px] rounded-full py-1 text-center text-white">
                   이번주 작품
                 </span>
@@ -129,11 +129,11 @@ const EventPage = () => {
                   CGV 5만원 상품권(1인)
                 </span>
               </div>
-              <div className="pretendard flex items-baseline gap-3 md:flex-row">
-                <span className="bg-footerBlue w-[90px] rounded-full py-1 text-center text-white">
+              <div className="pretendard flex flex-col items-baseline gap-3 md:flex-row">
+                <span className="bg-footerBlue hidden w-[90px] rounded-full py-1 text-center text-white md:block">
                   참여방법
                 </span>
-                <span className="text-sm md:text-base">
+                <span className="mt-3 text-center text-sm md:mt-0 md:text-left md:text-base">
                   이 페이지에서 대기해 주세요.
                   <br />
                   이벤트 시간이 되면 자동으로 버튼이 활성화됩니다!
