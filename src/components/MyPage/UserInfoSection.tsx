21ba079b4bd04579c59178b111e79b0b984db1c0
Lacheln <psionict@naver.com>
2025-08-01 13:29:26 +0900

    design 마이페이지 userInfoSection 디자인 수정

--- src/components/MyPage/UserInfoSection.tsx
+++ src/components/MyPage/UserInfoSection.tsx
@@ -121,7 +121,7 @@ const UserInfoSection: React.FC<UserInfoSectionProps> = ({
 
   return (
     <div className="pretendard relative sm:top-3 md:top-4">
-      <div className="flex w-full flex-col justify-center gap-8 pl-6">
+      <div className="md:g flex w-full flex-col justify-center gap-4 pl-6 md:flex-row md:justify-start md:gap-40">
         {/* 사용자 프로필 */}
         <div className="relative flex items-center">
           <div className="relative">
@@ -181,8 +181,13 @@ const UserInfoSection: React.FC<UserInfoSectionProps> = ({
           )}
         </div>
 
-        {/* 페르소나 정보 */}
+        {/* 중간 선 */}
         <div className="flex items-center">
+          <div className="hidden md:inline md:h-12 md:w-px md:bg-gray-600" />
+        </div>
+
+        {/* 페르소나 정보 */}
+        <div className="flex items-center md:right-52">
           <div>
             <img
               src={
