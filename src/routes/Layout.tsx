9381d62c7d5998b2fe23b71fe33f520a1a1a4554
yeeun426 <thsudkcla7@naver.com>
2025-08-05 23:26:20 +0900

    feat : 퀴즈 SSE 틀

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -3,8 +3,10 @@ import Footer from "../components/common/Footer";
 import Header from "@/components/common/Header";
 import useAuthCheck from "@/hooks/useAuthCheck";
 import Spinner from "@/components/common/Spinner";
+import { useEffect, useState } from "react";
 
 const Layout = () => {
+  const [notification, setNotification] = useState<null | string>(null);
   const { user, isLoading, logout } = useAuthCheck();
   const navigate = useNavigate();
 
@@ -15,6 +17,28 @@ const Layout = () => {
   const handleLogout = () => {
     logout();
   };
+
+  useEffect(() => {
+    if (!user?.userId) return;
+
+    const eventSource = new EventSource(
+      `/notifications/stream?clientId=${user.userId}`,
+    );
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setNotification(data.message);
+    };
+
+    eventSource.onerror = (error) => {
+      console.error("SSE 연결 오류:", error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, [user?.userId]);
 
   if (isLoading) {
     return (
@@ -30,9 +54,14 @@ const Layout = () => {
       <main className="flex-grow">
         <Outlet />
       </main>
-      <button className="fixed bottom-10 right-0 z-50 w-[80px] animate-bounce-chatbot sm:bottom-12 sm:right-10 sm:w-[130px] lg:right-28">
-        <img src="/images/main/chatbot.svg" alt="chatbot" />
-      </button>
+      {notification && (
+        <button
+          onClick={() => navigate("/event")}
+          className="fixed bottom-10 right-0 z-50 w-[80px] animate-bounce-chatbot sm:bottom-12 sm:right-10 sm:w-[130px] lg:right-28"
+        >
+          <img src="/images/popco/quiz.svg" alt="quiz" />
+        </button>
+      )}
       <Footer />
     </div>
   );
